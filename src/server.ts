e7397ae281d17a0035363e71d8e4986d27d95c33
Juliana Cristina <julianacristina780@gmail.com>
2020-06-12 19:12:59 -0300

    Criação dos Controllers e Rotas

--- src/server.ts
+++ src/server.ts
@@ -1,41 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
-
+app.use(cors())
 app.use(express.json())
 //Request Param: Parâmetros que vem na própria rota que identificam um recurso
 //Query Param: Parâmetros que vem na própria rota geralmente opcionais para fitros, paginação
 //Request Body: Parâmetros para criação/atualização de informações
-const users = [
-    'Juliana'
-];
+app.use(routes);
 
-app.get('/users', (request, response) => {
-
-    const search = String(request.query.search);
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    response.json(filteredUsers );
-});
-
-app.get('/users/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    const user = users[id]
-
-    return response.json(user);
-})
-
-app.post('/users', (request, response) => {
-
-    const data = request.body;
-
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user)
-})
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
