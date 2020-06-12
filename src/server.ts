d01efa7c651244cc35cb7cdc4f33687bb429331a
Juliana Cristina <julianacristina780@gmail.com>
2020-06-12 14:31:52 -0300

    Exemplos de rotas e filtro

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem de usuários');
+app.use(express.json())
+//Request Param: Parâmetros que vem na própria rota que identificam um recurso
+//Query Param: Parâmetros que vem na própria rota geralmente opcionais para fitros, paginação
+//Request Body: Parâmetros para criação/atualização de informações
+const users = [
+    'Juliana'
+];
 
-    response.json([
-        'Juliana'
-    ]);
+app.get('/users', (request, response) => {
+
+    const search = String(request.query.search);
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    response.json(filteredUsers );
 });
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id]
+
+    return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+
+    const data = request.body;
+
+    const user = {
+        name: data.name,
+        email: data.email
+    };
+
+    return response.json(user)
+})
 
 app.listen(3333);
