fd55588498f6bc2a5b40251599fed342bbf8ad48
Juliana Cristina <julianacristina780@gmail.com>
2020-06-12 11:32:35 -0300

    Primeira Aula NLW - Booster - Criação da API

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('Listagem de usuários');
+
+    response.json([
+        'Juliana'
+    ]);
+});
+
+app.listen(3333);
